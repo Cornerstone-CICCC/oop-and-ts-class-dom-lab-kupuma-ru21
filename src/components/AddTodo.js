073c587b26f09733b3262eb6579735a6faac3b32
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-09-19 14:28:50 -0700

    feat: done

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,13 +1,31 @@
 import { Component } from "../common/Component.js";
+import { TodoItem } from "./TodoItem.js";
 
 export class AddTodo extends Component {
   render() {
-    const addElement = document.createElement('div')
-    addElement.className = "add-todo"
-    addElement.innerHTML = `
-      <input type="text" id="todo-input" placeholder="Enter task details...">
-      <button id="todo-add-btn">Add To Do</button>
-    `
+    const addElement = document.createElement("div");
+    addElement.className = "add-todo";
+
+    const input = document.createElement("input");
+    input.id = "todo-input";
+    input.type = "text";
+    input.placeholder = "Enter task details...";
+    addElement.appendChild(input);
+
+    const addTodoButton = document.createElement("button");
+    addTodoButton.id = "todo-add-btn";
+    addTodoButton.textContent = "Add To Do";
+    addTodoButton.onclick = () => {
+      const todoElement = new TodoItem().render();
+      const span = document.createElement("span");
+      span.id = "todo-text";
+      span.textContent = input.value;
+      todoElement.insertBefore(span, todoElement.querySelector("#buttonGroup"));
+      const todos = document.getElementsByClassName("todo-list")[0];
+      todos.appendChild(todoElement);
+      input.value = "";
+    };
+    addElement.appendChild(addTodoButton);
 
     return addElement;
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,8 +2,41 @@ import { Component } from "../common/Component.js";
 
 export class TodoItem extends Component {
   render() {
-    const todoElement = document.createElement('li')
-    todoElement.className = "todo-item"
+    const todoElement = document.createElement("li");
+    todoElement.className = "todo-item";
+
+    const buttonGroup = document.createElement("div");
+    buttonGroup.id = "buttonGroup";
+    todoElement.appendChild(buttonGroup);
+
+    const markButton = document.createElement("button");
+    markButton.textContent = "Mark Complete";
+    markButton.onclick = () => {
+      const todoText = todoElement.querySelector("#todo-text");
+      if (markButton.textContent === "Mark Complete") {
+        markButton.textContent = "Mark Incomplete";
+        const s = document.createElement("s");
+        s.id = "incomplete-todo";
+        s.textContent = todoText.textContent;
+        todoText.replaceWith(s);
+        return;
+      }
+      markButton.textContent = "Mark Complete";
+
+      const s = todoElement.querySelector("#incomplete-todo");
+      const span = document.createElement("span");
+      span.id = "todo-text";
+      span.textContent = s.textContent;
+      s.replaceWith(span);
+    };
+    buttonGroup.appendChild(markButton);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.onclick = () => {
+      todoElement.remove();
+    };
+    buttonGroup.appendChild(deleteButton);
 
     return todoElement;
   }
